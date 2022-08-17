695da8f392e9f917891fc8a2ec3c24fb9afdb6f1
AlexandrMelnikov94 <76219280+AlexandrMelnikov94@users.noreply.github.com>
2022-08-17 14:44:53 +0300

    create react app

---
+++ tesla-clone-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tesla Clone</h1>
+      {/* Header */}
+      {/* HeaderBlock */}
+    </div>
+  );
+}
+
+export default App;
